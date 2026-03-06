9d4f7ca198eee0645fca29e2ed4bb4c982abbd45
Erik Hofmann <hofmannerik9@gmail.com>
2026-03-06 09:01:51 +0100

    added right url

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ function initStockfish() {
 
     return new Promise((resolve, reject) => {
         if (stockfish) sfTerminate();
-        stockfish = new Worker("stockfish-18.js#https://pub-166eb92c651049a9a716a84ec059e893.r2.dev");
+        stockfish = new Worker("stockfish-18.js#https://pub-166eb92c651049a9a716a84ec059e893.r2.dev/stockfishwasm/stockfish-18.wasm");
         stockfish.onerror = err => {
             console.error("Stockfish worker error:", err);
             const rej = sfPending?.reject || reject;
